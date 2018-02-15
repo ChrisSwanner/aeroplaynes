5ce4ccef3547e73d6357046dbe9b66803323c113
Chris Swanner and Joel Adams <chris@>
2018-02-15 08:17:00 -0800

    add all

---
+++ Template.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Template.Controllers;
+using Template.Models;
+
+namespace Template.Tests
+{
+  
+}
---
+++ Template/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Template.Models;
+
+namespace Template.Controllers
+{
+
+
+}
